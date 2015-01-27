40dc94dff17747f687a2c6c0f4d78c8d60d88cc5
Jahir Abrigo <jahir.abrigo@openebusiness.pe>
2015-01-27 16:36:55 -0500

    cambio agencia controller

--- eValorados-Web/Controllers/TipoAgenciaController.cs
+++ eValorados-Web/Controllers/TipoAgenciaController.cs
@@ -3,12 +3,103 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using eValorados_Web.Models;
+using eValorados_Web.Persistence;
+using NHibernate;
 
 namespace eValorados_Web.Controllers
 {
     public class TipoAgenciaController : Controller
     {
-        
+        private TipoAgenciaDAO _tipoAgenciaDAO = null;
+        private TipoAgenciaDAO TipoAgenciaDAO
+        {
+            get
+            {
+                if (_tipoAgenciaDAO == null)
+                    _tipoAgenciaDAO = new TipoAgenciaDAO();
+                return _tipoAgenciaDAO;
+            }
+        }
+        //
+        // GET: /TipoAgencia/
+        public ActionResult Index()
+        {
+            var _tipoAgencia = TipoAgenciaDAO.LoadAll();
+            return View(_tipoAgencia);
+        }
+
+        //
+        // GET: /TipoValorado/Details/5
+        public ActionResult Details(int id)
+        {
+            var _tipoAgencia = TipoAgenciaDAO.LoadById(id);
+            return View(_tipoAgencia);
+        }
+
+        //
+        // GET: /TipoValorado/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /TipoValorado/Create
+        [HttpPost]
+        public ActionResult Create(TipoAgencia tipoAgencia)
+        {
+            try
+            {
+                SessionHelper _sessionHelper = new SessionHelper();
+                using (ITransaction transaction = _sessionHelper.Current.BeginTransaction())
+                {
+                    TipoAgenciaDAO.Create(tipoAgencia);
+                    transaction.Commit();
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /TipoValorado/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var _TipoAgencia = TipoAgenciaDAO.LoadById(id);
+            return View(_TipoAgencia);
+        }
+
+        //
+        // POST: /TipoValorado/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, TipoAgencia TipoAgencia)
+        {
+            try
+            {
+                var _TipoAgencia = TipoAgenciaDAO.LoadById(id);
+                if (!TipoAgencia.IsActivo && _TipoAgencia.Agencias.Count > 0)
+                {
+                    ModelState.AddModelError("CustomError", String.Format("El tipo valorado con id=[{0}] esta siendo usado y no puede desactivarse", id));
+                    return View();
+                }
+
+                SessionHelper _sessionHelper = new SessionHelper();
+                using (ITransaction transaction = _sessionHelper.Current.BeginTransaction())
+                {
+                    TipoAgenciaDAO.Update(TipoAgencia);
+                    transaction.Commit();
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
 
 	}
 }
