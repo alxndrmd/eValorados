852b09ba30ac52337784f984345506d405138b06
Carlos Torres <carlos.torres@openebusiness.pe>
2015-02-05 13:54:03 -0500

    subire valorados

---
+++ eValorados-Web/Persistence/MovimientoDAO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using eValorados_Web.Models;
+
+namespace eValorados_Web.Persistence
+{
+    public class MovimientoDAO :BaseDao<Movimiento,int>
+    {//a
+        
+    }
+}
